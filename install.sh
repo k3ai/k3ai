bc35db4cb4e6ba621bceb8411b8aa190fd227daf
Alessandro Festa <afesta@alfweb.com>
2021-11-01 17:10:58 +0000

    install companion script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Downloading k3ai..."
+sleep 1
+
+wget https://github.com/k3ai/k3ai/releases/download/1.0/k3ai
+
+chmod +x k3ai
+
+sudo mv k3ai /usr/local/bin
+
+echo "Done. Check documenation at https://k3ai.in to start."
