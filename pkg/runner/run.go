1998605322afaf2358a3547c4e639ef788cff2d1
Alessandro Festa <80312181+alefesta@users.noreply.github.com>
2021-11-10 18:59:43 +0100

    Update run.go

--- pkg/runner/run.go
+++ pkg/runner/run.go
@@ -32,15 +32,22 @@ spec:
     spec:
       containers:
       - name: k3ai
-        image: ghcr.io/k3ai/k3ai-executor:latest
+        image: ghcr.io/k3ai/k3ai-executor:dev
         command: ["/bin/sleep", "3650d"]
 EOF
 `
 var k3aiKube = "/.tools/kubectl"
 
-func Loader(source string, target string, backend string, extras string) error {
-	var execTemplate = "\". ./run.sh -b " + backend + " -s " + source + "\" "
-	execTemplate = execTemplate + `
+
+func Loader(source string,target string,backend string, extras string, entrypoint string) error {
+	var execTemplate string
+if entrypoint == "" {
+	execTemplate ="\"/opt/k3ai-executor -b " + backend + " -s " + source  + "\" "
+	execTemplate = execTemplate+ "\nEOF"
+}
+
+var execTemplateKfp ="\"/opt/k3ai-executor -b " + backend + " -s " + source  + " -e " + entrypoint + "\" "
+execTemplateKfp = execTemplateKfp + `
 EOF
 `
 
@@ -48,19 +55,21 @@ EOF
 	out := factory.Client(name, ctype)
 	home, _ := os.UserHomeDir()
 	shellPath := home + "/.k3ai"
-	outcome, err := exec.Command("/bin/bash", "-c", "cat <<EOF | "+shellPath+k3aiKube+" apply  --kubeconfig="+out+" -f - "+template).Output()
-	if err != nil {
-		log.Println(err)
-	}
+  outcome,err := exec.Command("/bin/bash","-c", "cat <<EOF | " + shellPath + k3aiKube + " apply  --kubeconfig="+ out +" -f - " + template ).Output()
+  if err != nil {
+    log.Println(err)
+  }
 
-	fmt.Println(string(outcome))
+  exec.Command("/bin/bash","-c", shellPath + k3aiKube + " wait --for=condition=Ready pods --all -n default  --kubeconfig="+ out).Output()
+  fmt.Println(string(outcome))
 
-	time.Sleep(10 * time.Second)
-	outcome_new, _ := exec.Command("/bin/bash", "-c", shellPath+k3aiKube+" wait --for=condition=Ready pods --all --all-namespaces  --kubeconfig="+out).Output()
-	fmt.Println(string(outcome_new))
-	if err != nil {
-		log.Fatal(err)
-	}
+  time.Sleep(10 * time.Second)
+  
+  if backend == "mlflow" {
+	exec.Command("/bin/bash","-c",  "cat <<EOF | " + shellPath + k3aiKube + "  --kubeconfig="+ out + " exec  svc/minio-service -- bash -c \" mkdir /data/mlflow \"" ).Output()
+  }
+  exec.Command("/bin/bash","-c", shellPath + k3aiKube + " wait --for=condition=Ready pods --all -n default  --kubeconfig="+ out).Output()
+
 
 	if backend == "mlflow" {
 		cmd := exec.Command("/bin/bash", "-c", "cat <<EOF | "+shellPath+k3aiKube+"  --kubeconfig="+out+" exec  deployment/k3ai-executor -- bash -c "+execTemplate)
@@ -81,6 +90,39 @@ EOF
 			fmt.Printf("\r %v", msg)
 			fmt.Println(" ")
 			for scanner.Scan() {
+				line := scanner.Text()
+				color.Disable()
+				log.Println(line)
+			}
+			done <- struct{}{}
+		}()
+		// Start the command and check for errors
+		err := cmd.Start()
+		if err != nil {
+			log.Println("Something went wrong... did you check all the prerequisites to run this plugin? If so try to re-run the k3ai command...")	
+		}
+		<-done
+		err = cmd.Wait()
+		if err != nil {
+			log.Fatal(err)
+		}
+  }
+  if backend == "kfp" {
+    cmd:= exec.Command("/bin/bash","-c",  "cat <<EOF | " + shellPath + k3aiKube + "  --kubeconfig="+ out + " exec  deployment/k3ai-executor -- bash -c " + execTemplateKfp )
+    cmd.Dir = shellPath
+		r, _ := cmd.StdoutPipe()
+		cmd.Stderr = cmd.Stdout
+    done := make(chan struct{})
+
+		scanner := bufio.NewScanner(r)
+
+    // loader.Working(msg)
+    go func() {
+			// Read line by line and process it
+      msg := "🧪	Working, please wait..."
+      fmt.Printf("\r %v", msg)
+			fmt.Println(" ")
+			for scanner.Scan() {
 				scanner.Text()
 				color.Disable()
 			}
@@ -96,10 +138,7 @@ EOF
 		if err != nil {
 			log.Fatal(err)
 		}
-	}
-	// _,_ = exec.Command("/bin/bash","-c", "cat <<EOF | " + shellPath + k3aiKube + " delete  --kubeconfig="+ out +" -f - " + template ).Output()
-	// if err != nil {
-	//   log.Println(err)
-	// }
-	return nil
+  }
+  return nil
 }
+
